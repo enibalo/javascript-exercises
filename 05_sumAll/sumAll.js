ac133cb008ce64f5c8326456c435eb5cad850f91
enibalo <49178664+enibalo@users.noreply.github.com>
2023-05-22 22:25:25 -0600

    Solve assignment 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let total = 0;
+    if (typeof num1 != "number" || typeof num2 != "number" || num1 < 0 || num2 < 0){
+        return "ERROR";
+    }
+    if (num1 > num2){
+        for (let i = num2; i <= num1; i++){
+            total = total + i;
+        }
+    }
+    else{
+        for ( let i = num1; i <= num2; i++){
+            total = total + i;
+        }
+    }
+   return total;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+console.log(sumAll(1,"10"));
+
