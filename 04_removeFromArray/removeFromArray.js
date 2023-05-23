967d95c6554d3507cfe7eaa56fb82efea6d21aaf
enibalo <49178664+enibalo@users.noreply.github.com>
2023-05-22 21:43:37 -0600

    Finish assignment 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    let elements = Array.from(arguments);
+    let list = elements.slice(1);
+    function getStrippedArray(element){
+        let answer = (list.indexOf(element) == -1)?true:false;
+        return answer;
+    }
+    return array.filter(getStrippedArray);
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+console.log(removeFromArray([1,2,3,4], 2, "hi"))
