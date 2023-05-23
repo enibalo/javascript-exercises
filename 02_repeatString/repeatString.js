5e109a78f05c425c30bb6c09ba97c8eca1242351
enibalo <49178664+enibalo@users.noreply.github.com>
2023-05-22 19:58:21 -0600

    Finish assignment 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function( word, number ) {
+    let answer = '';
+    for ( let i = 0; i < number; i++ ){
+        answer = answer + word;
+    }
+    answer = (number < 0)? "ERROR":answer;
+    return answer;
 };
 
 // Do not edit below this line
