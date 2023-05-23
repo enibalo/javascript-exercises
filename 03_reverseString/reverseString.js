2c5efb04dc9427bd9f7cbc7983cbef03aad1433a
enibalo <49178664+enibalo@users.noreply.github.com>
2023-05-22 21:05:51 -0600

    Finish assignment 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let answer = "";
+    for ( let i = word.length -1; i > -1; i--){
+        answer += word[i];
+    }
+    return answer;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+console.log( reverseString("hello world!"));
