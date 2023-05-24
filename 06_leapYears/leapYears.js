46a755511535ded18fee2357ca3dc6955d98d884
enibalo <49178664+enibalo@users.noreply.github.com>
2023-05-24 10:38:32 -0600

    Finish assignment 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let answer = (year % 4 == 0)?true: false;
+    if (answer == true && year % 400 != 0 && year % 100 == 0){
+        answer = false;
+    }
+    return answer;
 };
 
 // Do not edit below this line
