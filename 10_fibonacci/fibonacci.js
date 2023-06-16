254d25eb55e59eedfa03c4941b390080f4a82ee5
enibalo <49178664+enibalo@users.noreply.github.com>
2023-06-16 16:03:41 -0600

    Solve all problems

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if (index <= 0){
+        return "OOPS";
+    } 
+    
+    let answer = 1;
+    let prev_answer = 1;
+    let prev2_answer = 0;
+    for (let i = 1; i < index; i++){
+        answer = prev2_answer + prev_answer;
+        prev2_answer = prev_answer;
+        prev_answer = answer;
+    }
+    return answer;
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+console.log("i");
+console.log(fibonacci(-1));
