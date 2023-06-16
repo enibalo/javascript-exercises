5a10cbfbbf81c83fb21acbd88e76ea79b546558a
enibalo <49178664+enibalo@users.noreply.github.com>
2023-06-16 03:47:27 -0600

    Solve all problems

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  array = array.map((element)=> Number(element));
+  return array.reduce((total, year) => total + year, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce( (total,year)=> total * year, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let answer = 1;
+  for ( let i = 2; i <= num; i ++){
+  answer = answer * i;
+  }
+  return answer;
 };
 
 // Do not edit below this line
@@ -31,3 +36,6 @@ module.exports = {
   power,
   factorial
 };
+
+let num = factorial(3);
+console.log(num);
