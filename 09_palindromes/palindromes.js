ac2c760c5bffd4f0b341532b9b2d43b7c00ba14a
enibalo <49178664+enibalo@users.noreply.github.com>
2023-06-16 15:24:59 -0600

    Solve all problems

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
-
-};
+const palindromes = function (word) {
+    // indIndex() filter()
+    let myWord = Array.from(word);
+    let arr = myWord.filter(function(element){
+        if (element.match(/[a-z]/i)){
+            return true;
+        }
+        return false;
+    });
+    let arr2 = [...arr];
+    return arr.every( function(element){
+        if (arr2.at(-1).toUpperCase() == element.toUpperCase()){
+            arr2.pop();
+            return true;
+        }else{
+            arr2.pop();
+            return false;
+        }
+    });
+}
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
+console.log(palindromes("aa a"));
